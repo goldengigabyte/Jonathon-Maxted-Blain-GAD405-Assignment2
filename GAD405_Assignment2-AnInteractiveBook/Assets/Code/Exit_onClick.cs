169ecfac58434884c708a73ec1930ae082a3ac19
Jon Maxted <jonnie.maxted@gmail.com>
2019-01-02 23:08:02 +0000

    my god...

---
+++ GAD405_Assignment2-AnInteractiveBook/Assets/Code/Exit_onClick.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_onClick : MonoBehaviour {
+	void Exit(){
+		Application.Quit();
+	}
+}
---
+++ GAD405_Assignment2-AnInteractiveBook/Assets/Code/Start_onPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start_onPress : MonoBehaviour {
+	public GameObject Open_Menu;
+	public GameObject UI_KeepAlive;
+	public Text Message;
+	private string Score;
+	public Text Score_Out;
+	public string Next_Scene;
+
+	public void Awake(){
+		UI_KeepAlive.gameObject.SetActive(false);
+		DontDestroyOnLoad(UI_KeepAlive);
+		Score = "NaN40404";
+	}
+	private void FixedUpdate(){
+		Score_Out.text = Score;
+	}
+	public void Start_Game(){
+        SceneManager.LoadScene(Next_Scene);
+        Open_Menu.SetActive(false);
+		UI_KeepAlive.SetActive(true);
+    }
+}
